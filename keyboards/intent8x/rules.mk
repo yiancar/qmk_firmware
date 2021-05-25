1219b658b5750e574596014439e5545994bfa617
yiancar <yiancar@gmail.com>
2021-05-26 00:46:08 +0100

    disabled consol

--- keyboards/intent8x/rules.mk
+++ keyboards/intent8x/rules.mk
@@ -13,7 +13,7 @@ OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
