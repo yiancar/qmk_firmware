48086f023aefab58997787f368b5fd195f5ed244
yiancar <yiancar@gmail.com>
2022-01-24 18:34:59 +0000

    update rules.mk

--- keyboards/hyprkeys_alpha8x/rules.mk
+++ keyboards/hyprkeys_alpha8x/rules.mk
@@ -1,22 +1,26 @@
 # MCU name
 MCU = STM32F072
 
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+
+# Wildcard to allow APM32 MCU
+DFU_SUFFIX_ARGS = -v FFFF -p FFFF
+
 # Do not put the microcontroller into power saving mode
 # when we get USB suspend event. We want it to keep updating
 # backlight effects.
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BACKLIGHT_ENABLE = no              # Enable keyboard backlight functionality
-BOOTMAGIC_ENABLE = lite            # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes              # Mouse keys
-EXTRAKEY_ENABLE = yes              # Audio control and System control
-CONSOLE_ENABLE = no                # Console for debug
-COMMAND_ENABLE = no                # Commands for debug and configuration
-SLEEP_LED_ENABLE = no              # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes                  # USB Nkey Rollover
-AUDIO_ENABLE = no                  # Audio output on port C6
-NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in
-RGBLIGHT_ENABLE = no               # Underglow RGB
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
