ced2f8505115412f78a31f63aa463f1cdeccfa65
yiancar <yiangosyiangou@cytanet.com.cy>
2026-09-23 20:19:19 +0100

    initial odinn

---
+++ keyboards/novelkeys/odinn/config.h
@@ -0,0 +1,8 @@
+// Copyright 2026 Yiancar
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define BACKLIGHT_PWM_DRIVER PWMD3
+#define BACKLIGHT_PWM_CHANNEL 3
+#define BACKLIGHT_PAL_MODE 1
